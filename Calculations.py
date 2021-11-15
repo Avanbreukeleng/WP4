4e6edd53af8d48e52f8717f6f4ecb25347ef2f5a
Avanbreukeleng <92540385+Avanbreukeleng@users.noreply.github.com>
2021-11-15 15:10:40 +0100

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -1,1 +1,7 @@
 #NEW File
+import numpy as np
+
+L = 100
+
+t = np.linspace(10**-3,0.02,L) #thickness from 1mm to 2 cm
+w_small = np.linspace(10**-3,0.03,L) #distance between inner hole and outer radius
