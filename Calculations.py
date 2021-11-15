5dbe4198b74385d4b342f895aaa4de0256d895d6
Avanbreukeleng <92540385+Avanbreukeleng@users.noreply.github.com>
2021-11-15 15:36:44 +0100

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -7,8 +7,24 @@ t = np.linspace(10**-3,0.02,L) #thickness from 1mm to 2 cm
 w_small = np.linspace(10**-3,0.03,L) #distance between inner hole and outer radius
 D = np.linspace(0.01,0.10,L) #hole diameter
 W = D + w_small
+e = w_small + D/2
+eD = e/D
+WD = W/D
+At = (W-D)*t
+Abr = D*t
+#Stress concentration
+kt = 1
+kbr = 1
+kty = 1
+#Material properties
+Fty = 414 * 10**6 #Pa
+Ftu = 483 * 10**6 #Pa
+density = 2800 #kg/m^3
 
-
+#Allowable forces
+Py = kt * Fty * At
+Pbry = kbr * Ftu * Abr
+Pty = kty * Abr * Fty
 
 
 A1=t*(W-D/np.sqrt(2))/2
