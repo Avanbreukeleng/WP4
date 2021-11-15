fbba6fbfc1ab040b841629f9fe3779652d9b854b
koadfffff <94373462+koadfffff@users.noreply.github.com>
2021-11-15 15:14:36 +0100

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -7,3 +7,8 @@ t = np.linspace(10**-3,0.02,L) #thickness from 1mm to 2 cm
 w_small = np.linspace(10**-3,0.03,L) #distance between inner hole and outer radius
 D = np.linspace(0.01,0.10,L) #hole diameter
 W = D + w_small
+
+
+
+
+A1=t*(W-D/np.sqrt(2))/2
